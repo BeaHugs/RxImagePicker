d36889d643c0ea61b0e36ffbbed598e1e5ce1be7
Bea <1257924355@com>
2018-04-08 10:13:41 +0800

    commit code

--- CustomView/app/src/main/java/com/k2/custom/MainActivity.java
+++ CustomView/app/src/main/java/com/k2/custom/MainActivity.java
@@ -1,7 +1,11 @@
 package com.k2.custom;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -36,6 +40,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         data();
+        Log.i("xxx","手机型号:" + isAppOnForeground());
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("xxx","onRestart:" + isAppOnForeground());
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("xxx","onResume:" + isAppOnForeground());
     }
 
     // 数据
@@ -45,5 +62,29 @@ public class MainActivity extends AppCompatActivity {
             strList.add(RandomMath.math(1, MAX));
         }
         frist_view.setData(strList);
+    }
+    /**
+     * 程序是否在前台运行
+     * @return
+     */
+    public boolean isAppOnForeground() {
+        // Returns a list of application processes that are running on the device
+        ActivityManager activityManager = (ActivityManager) getApplicationContext()
+                .getSystemService(Context.ACTIVITY_SERVICE);
+        String packageName = getApplicationContext().getPackageName();
+
+        List<ActivityManager.RunningAppProcessInfo> appProcesses = activityManager
+                .getRunningAppProcesses();
+        if (appProcesses == null){
+            return false;
+        }
+        for (ActivityManager.RunningAppProcessInfo appProcess : appProcesses) {
+            // The name of the process that this object is associated with.
+            if (appProcess.processName.equals(packageName)
+                    && appProcess.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+                return true;
+            }
+        }
+        return false;
     }
 }
